b6a5462ba657926c365299133797f4055bcd70d1
Liam <Liamfosterlff@gmail.com>
2020-04-17 18:51:03 +0100

    moved the information for the modal outside of modal so that different modals have different info

--- bug-project/src/components/KanbanTab/index.js
+++ bug-project/src/components/KanbanTab/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { v4 as uuid } from 'uuid';
-import Modal from './Modal';
+import Modal from '../Modal';
 
 // fake data generator
 const getItems = count =>
@@ -22,6 +22,7 @@ const getItemStyle = (isDragging, draggableStyle) => ({
     background: "white",
     cursor: "pointer",
     boxShadow: isDragging ? "10px 10px 10px #e3e3e5" : "none",
+    fontSize: "10px",
 
     // styles we need to apply on draggables
     ...draggableStyle
@@ -40,25 +41,41 @@ const KanbanTab = () => {
     const columnsFromBackend = {
         [uuid()]: {
             name: "Backlog",
-            items: getItems(3)
+            issues: [
+                {
+                    id: uuid(),
+                    card: {
+                        currentIssueType: "Bug",
+                        title: { value: "Each issue has a single reporter but can have multiple assignees." },
+                        value: 'Try assigning Pickle Rick to this issue. 🥒 🥒 🥒',
+                        commentsText: [{ user: { username: "Lord Gaben", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" }, date: "3 days ago", text: "In the moonlight, The color and scent of the wisteria Seems far away." }],
+                        estimatedTime: 6,
+                        currentStatusType: "backlog",
+                        currentPriorityType: "High",
+                        currentReportingUser: { username: "Baby Yoda", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" },
+                        currentAssignedUsers: [{ username: "Baby Yoda", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" }, { username: "Pickle Rick", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" }]
+                    }
+                }, 
+                
+            ]
         },
         [uuid()]: {
             name: "Selected for Development",
-            items: getItems(2)
+            issues: []
         },
         [uuid()]: {
             name: "In Progress",
-            items: getItems(2)
+            issues: []
         },
         [uuid()]: {
             name: "Done",
-            items: getItems(2)
+            issues: []
         }
 
     }
 
 
-
+    // { id: uuid(), content: "Hook up frontend and backend, so that chart is populated by a backend server" } { id: uuid(), content: "Hook up all data so it goes through from kanbantab page object to Modal Object" }, { id: uuid(), content: "Clean up functions and components into their own separate components" }
 
     const [modalIsOpen, setModalIsOpen] = useState(false)
 
@@ -107,11 +124,21 @@ const KanbanTab = () => {
 
     }
     
+    const [currentModalCard, setCurrentModalCard] = useState({
+        currentIssueType: "",
+        title: { value: "" },
+        value: '',
+        commentsText: [{ user: { username: "", avatar: "" }, date: "", text: "" }],
+        estimatedTime: 0,
+        currentStatusType: "",
+        currentPriorityType: "",
+        currentReportingUser: { username: "", avatar: "" },
+        currentAssignedUsers: [{ username: "", avatar: "" }]
+    })
 
-    const handleIssueClick = (e, column) => {
-        console.log(column, column.items.filter(item => item.id === e.target.dataset.rbdDraggableId))
+    const handleIssueClick = (e, column, issue) => {
         setModalIsOpen(true)
-        
+        setCurrentModalCard(issue.card)
     }
 
     
@@ -131,11 +158,11 @@ const KanbanTab = () => {
                                         ref={provided.innerRef}
                                         style={getListStyle(snapshot.isDraggingOver)}
                                     >
-                                        {column.items.map((item, index) => (
-                                            <Draggable key={item.id} draggableId={item.id} index={index}>
+                                        {column.issues.map((issue, index) => (
+                                            <Draggable key={issue.id} draggableId={issue.id} index={index}>
                                                 {(provided, snapshot) => (
                                                     <div
-                                                        onClick={e => handleIssueClick(e, column)}
+                                                        onClick={e => handleIssueClick(e, column, issue)}
                                                         ref={provided.innerRef}
                                                         {...provided.draggableProps}
                                                         {...provided.dragHandleProps}
@@ -144,7 +171,7 @@ const KanbanTab = () => {
                                                             provided.draggableProps.style
                                                         )}
                                                     >
-                                                        {item.content}
+                                                        {issue.card.title.value}
                                                     </div>
                                                 )}
                                             </Draggable>
@@ -159,7 +186,7 @@ const KanbanTab = () => {
                 })}
             </DragDropContext>
         </div>
-        <Modal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}></Modal>
+        <Modal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} card={currentModalCard}></Modal>
         
     </div>
       
--- bug-project/src/components/Modal/index.js
+++ bug-project/src/components/Modal/index.js
@@ -1,11 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle, faStopwatch, faCheckSquare, faExclamationCircle, faBookmark, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import TextareaAutosize from 'react-textarea-autosize';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import gaben from './gaben.jpg';
+import gaben from '../gaben.jpg';
+import { v4 as uuid } from 'uuid';
 
 const ModalBackdrop = styled.div`
     display: ${props => props.modalIsOpen ? "inherit" : "none"};
@@ -315,16 +316,242 @@ const PriorityTypePicker = styled.div`
     }
 `;
     
-const Modal = (props) => {
+const ReportingUserPicker = styled.div`
+    font-size: 10px;
+    letter-spacing: 0.5px;
+    color: rgb(94, 108, 132);
+    font-family: CircularStdBold;
     
+    .priority-type-icon-box svg {
+        &[data-priority-type="Highest"] {
+            color: darkred;
+        }
+        &[data-priority-type="High"] {
+            color: red;
+        }
+        &[data-priority-type="Medium"] {
+            color: orange;
+        }
+        &[data-priority-type="Low"] {
+            color: green;
+        }
+        &[data-priority-type="Lowest"] {
+            color: darkgreen;
+        }
+    }
+
+    .priority-type-text {
+        padding: 5px;
+        display: inline-block;
+        border-radius: 3px;
+        margin: 2px;
+        
+    }
+
+    .current-type {
+        display: inline-block;
+        border-radius: 3px;
+        padding: 2px;
+        &:hover {
+            background-color: #EBECF0;
+        }
+    }
+
+    .dropdown {
+        display: ${props => props.showReportingUserPicker ? "block" : "none"};
+        position: absolute;
+        background-color: #ffffff;
+        min-width: 80px;
+        box-shadow: 0px 8px 8px 0px rgba(0,0,0,0.15);
+        z-index: 1;
+
+        .option {
+            width: 100%;
+            display: inline-block;
+            cursor: pointer;
+            &:hover {
+                background-color: #D3E5FE;
+            }
+        }
+    }
+`;
+
+const AssignedUsersPicker = styled.div`
+    font-size: 10px;
+    letter-spacing: 0.5px;
+    color: rgb(94, 108, 132);
+    font-family: CircularStdBold;
+    
+    .priority-type-icon-box svg {
+        &[data-priority-type="Highest"] {
+            color: darkred;
+        }
+        &[data-priority-type="High"] {
+            color: red;
+        }
+        &[data-priority-type="Medium"] {
+            color: orange;
+        }
+        &[data-priority-type="Low"] {
+            color: green;
+        }
+        &[data-priority-type="Lowest"] {
+            color: darkgreen;
+        }
+    }
+
+    .priority-type-text {
+        padding: 5px;
+        display: inline-block;
+        border-radius: 3px;
+        margin: 2px;
+        
+    }
+
+    .current-type {
+        display: inline-block;
+        border-radius: 3px;
+        padding: 2px;
+        &:hover {
+            background-color: #EBECF0;
+        }
+    }
+
+    .dropdown {
+        display: ${props => props.showAssignedUserPicker ? "block" : "none"};
+        position: absolute;
+        background-color: #ffffff;
+        min-width: 80px;
+        box-shadow: 0px 8px 8px 0px rgba(0,0,0,0.15);
+        z-index: 1;
+
+        .option {
+            width: 100%;
+            display: inline-block;
+            cursor: pointer;
+            &:hover {
+                background-color: #D3E5FE;
+            }
+        }
+    }
+`;
+
+
+const BigAvatar = styled.div`
+    background-image: url(${ props => props.image });
+    background-size: cover; 
+    width: 24px; 
+    height: 24px; 
+    display: block;
+    border-radius: 100%;
+`;
+
+const SmallAvatar = styled.div`
+    background-image: url(${ props => props.image});
+    background-size: cover; 
+    width: 15px; 
+    height: 15px; 
+    display: block;
+    border-radius: 100%;
+`;
+
+const AddCommentsForm = styled.div`
+    postion: relative;
+
+
+
+    textarea {
+        padding-left: 44px;
+        border: 1px solid #e3e3e5;
+        border-radius: 2px;
+        overflow: hidden;
+        outline: none;
+        resize: none;
+        background-color: #F4F5F7;
+        
+
+        &:focus {
+            background-color: #FFFFFF;
+            border-color: #0647A6;
+        }
+    }
+
+    .save-button {
+        display: ${props => props.show ? "inline-block" : "none"};
+        background-color: #0647A6;
+        color: white;
+        padding: 6px;
+        margin: 2px;
+        border-radius: 2px;
+    }
+
+    .cancel-button {
+        display: ${props => props.show ? "inline-block" : "none"};
+        padding: 6px;
+        margin: 2px;
+        border-radius: 2px;
+        &:hover {
+            background-color: #EBECF0;
+        }
+    }
+`;
+
+
+const CommentsDisplay = styled.div`
+
+`;
+
+const Modal = (props) => {
+    console.log(props.card.currentIssueType);
+    
+    const [showIssuePicker, setShowIssuePicker] = useState(false)
+    const [showCommentsButtons, setShowCommentsButtons] = useState(false)
+    const [showDescriptionTextEditor, setShowDescriptionTextEditor] = useState(false)
+    const [showStatusPicker, setShowStatusPicker] = useState(false)
+    const [showPriorityPicker, setShowPriorityPicker] = useState(false)
+    const [showReportingUserPicker, setShowReportingUserPicker] = useState(false)
+    const [showAssignedUserPicker, setShowAssignedUserPicker] = useState(false)
+
+
+    useEffect(() => {
+        setCurrentIssueType(props.card.currentIssueType)
+         setCurrentIssueType(props.card.currentIssueType)
+         setTitle(props.card.title)
+         setValue(props.card.value);
+         setAddCommentText({ value: "" })
+         setCommentsText(props.card.commentsText)
+         setEstimatedTime(props.card.estimatedTime)
+         setCurrentStatusType(props.card.currentStatusType)
+         setCurrentPriorityType(props.card.currentPriorityType)
+         setCurrentReportingUser(props.card.currentReportingUser)
+         setCurrentAssignedUsers(props.card.currentAssignedUsers)
+    }, [props.card])
+
+
+
+    const [currentIssueType, setCurrentIssueType] = useState(props.card.currentIssueType)
+    const [title, setTitle] = useState(props.card.title)
+    const [value, setValue] = useState(props.card.value);
+    const [addCommentText, setAddCommentText] = useState({ value: "" })
+    const [commentsText, setCommentsText] = useState(props.card.commentsText)
+    const [estimatedTime, setEstimatedTime] = useState(props.card.estimatedTime)
+    const [currentStatusType, setCurrentStatusType] = useState(props.card.currentStatusType)
+    const [currentPriorityType, setCurrentPriorityType] = useState(props.card.currentPriorityType)
+    const [currentReportingUser, setCurrentReportingUser] = useState(props.card.currentReportingUser)
+    const [currentAssignedUsers, setCurrentAssignedUsers] = useState(props.card.currentAssignedUsers)
+    console.log(currentIssueType);
+    
+
+
     const modalBox = useRef()
     const issuePicker = useRef()
     const statusPicker = useRef()
     const priorityPicker = useRef()
-    const [showIssuePicker, setShowIssuePicker] = useState(false)
-
-    const [value, setValue] = useState('Try assigning Pickle Rick to this issue. 🥒 🥒 🥒');
-    const [showDescriptionTextEditor, setShowDescriptionTextEditor] = useState(false)
+    const reportingUserPicker = useRef()
+    const assignedUserPicker = useRef()
+    
+    
+    
     const handleOutsideClicks = (e) => { //Handles all outside clicks for entire modal, since backdrop covers entire screen
         if (!modalBox.current.contains(e.target)) {
             props.setModalIsOpen(false)
@@ -340,6 +567,14 @@ const Modal = (props) => {
 
         if (!priorityPicker.current.contains(e.target)) {
             setShowPriorityPicker(false)
+        }
+
+        if (!reportingUserPicker.current.contains(e.target)) {
+            setShowReportingUserPicker(false)
+        }
+
+        if (!assignedUserPicker.current.contains(e.target)) {
+            setShowAssignedUserPicker(false)
         }
     }
      
@@ -359,19 +594,13 @@ const Modal = (props) => {
         
     }
 
-    const [title, setTitle] = useState({value: "Each issue has a single reporter but can have multiple assignees."})
-
+    
     const handleTitleChange = (e) => {
         setTitle({value: e.target.value});
         
     };
 
-    const [estimatedTime, setEstimatedTime] = useState(6)
     
-
-    
-
-    const [currentIssueType, setCurrentIssueType] = useState("Bug")
 
     const issueTypeRender = (currentIssueType) => {
 
@@ -409,8 +638,7 @@ const Modal = (props) => {
     }
 
 
-    const [currentStatusType, setCurrentStatusType] = useState("backlog")
-    const [showStatusPicker, setShowStatusPicker] = useState(false)
+    
     const statusTypeRender = (currentIssueType) => {
 
         const handleOptionClick = (e, type) => {
@@ -446,8 +674,7 @@ const Modal = (props) => {
     }
 
 
-    const [currentPriorityType, setCurrentPriorityType] = useState("High")
-    const [showPriorityPicker, setShowPriorityPicker] = useState(false)
+    
     const priorityTypeRender = (currentIssueType) => {
 
         const handleOptionClick = (e, type) => {
@@ -488,7 +715,128 @@ const Modal = (props) => {
             </div>
         )
     }
+
     
+    const reportingUserRender = (currentReportingUser) => {
+
+        const handleOptionClick = (e, user) => {
+            setCurrentReportingUser(user)
+            setShowReportingUserPicker(false)
+        }
+        const users = [
+            { username: "Baby Yoda", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" },
+            { username: "Pickle Rick", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" },
+            { username: "Lord Gaben", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg" }
+        ]
+
+        const options = users.filter(user => user.username !== currentReportingUser.username)
+        const optionsJSX = options.map(user => {
+            return (
+                <div className="option" onClick={(e) => handleOptionClick(e, user)}>
+                    <div className="reporting-user-avatar-box">
+                        <SmallAvatar image={user.avatar} />
+                        <div className="reporting-user-name" >{user.username}</div>
+                    </div>
+                </div>
+            )
+        })
+
+        return (
+            <div>
+                <div className="current-reporting-user" onClick={e => setShowReportingUserPicker(true)}>
+                    <div className="reporting-user-avatar-box">
+                        <SmallAvatar image={currentReportingUser.avatar}/>
+                        <div className="reporting-user-name" >{currentReportingUser.username}</div>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    {optionsJSX}
+                </div>
+            </div>
+        )
+    }
+    
+
+    
+    const assignedUserRender = (currentAssignedUsers) => {
+
+        const handleOptionClick = (e, user) => {
+            setCurrentAssignedUsers([...currentAssignedUsers, user])
+            setShowAssignedUserPicker(false)
+        }
+
+        const handleRemoveUserClick = (e, removeUser) => {
+            console.log(removeUser);
+            
+            const copiedCurrentAssignedUsers = [...currentAssignedUsers];
+            const newCurrentUsersList = copiedCurrentAssignedUsers.filter((currentUser) => currentUser.username !== removeUser.username)
+            setCurrentAssignedUsers(newCurrentUsersList)
+        }
+
+        const users = [
+            { username: "Baby Yoda", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg", id: uuid() },
+            { username: "Pickle Rick", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg", id: uuid() },
+            { username: "Lord Gaben", avatar: "https://i.ibb.co/6n0hLML/baby-yoda.jpg", id: uuid() }
+        ]
+
+        const options = users.filter(user => {
+            const currentAssignedUsernames = currentAssignedUsers.map(currentUser => currentUser.username)
+            return !(currentAssignedUsernames.includes(user.username))
+        })
+        const optionsJSX = options.map(user => {
+            return (
+                <div className="option" onClick={(e) => handleOptionClick(e, user)}>
+                    <div className="reporting-user-avatar-box">
+                        <SmallAvatar image={user.avatar} />
+                        <div className="reporting-user-name" >{user.username}</div>
+                    </div>
+                </div>
+            )
+        })
+
+        const currentAssignedUsersJSX = currentAssignedUsers.map(user => {
+            return (
+                <div className="current-assigned-user">
+                    <div className="assigned-user-avatar-box">
+                        <SmallAvatar image={user.avatar} />
+                        <div className="assigned-user-name" >{user.username}</div>
+                        <div className="remove-assigned-user-btn" onClick={(e) => handleRemoveUserClick(e, user)}>X</div>
+                    </div>
+                </div>
+            )
+        })
+
+        return (
+            <div>a
+                {currentAssignedUsersJSX}
+                <div className="add-assigned-user-button" onClick={e => setShowAssignedUserPicker(true)}>Add more +</div>
+                <div className="dropdown">
+                    {optionsJSX}
+                </div>
+            </div>
+        )
+    }
+
+
+    const commentsContentRender = commentsText.map((comment) => {
+        return (
+            <div>
+                <div className="avatar-icon-box">
+                    <BigAvatar image={comment.user.avatar} />
+                    <div className="username">{comment.user.username}</div>
+                </div>
+                <div className="comment-content">
+                    <div className="comment-date">{comment.date}</div>
+                    <div className="comment-text">{comment.content}</div>
+                    <div className="edit-btn">Edit</div>
+                    <div className="delete-btn">Delete</div>
+                </div>
+            </div>
+        )
+    })
+    
+
+
     return (
         <div>
         <ModalBackdrop modalIsOpen={props.modalIsOpen} onMouseDown={(e) => handleOutsideClicks(e)}>
@@ -527,10 +875,22 @@ const Modal = (props) => {
                             <div className="cancel-button">Cancel</div>
                         </IssueDescription>
                         <div className="issue-comments">Comments
-                            <div className="comments-form">Add a comment
-                                <div className="comments-tip">Press m for magic</div>
-                            </div>
-                            <div className="comments-display">Lord Gaben3 days ago In the moonlight,The color and scent of the wisteria Seems far away. Edit Delete</div>
+                            <AddCommentsForm show={showCommentsButtons}>
+                                <BigAvatar className="big-avatar" image={gaben} />
+                                <TextareaAutosize
+                                    key="input"
+                                    placeholder="Add a comment"
+                                    value={addCommentText.value}
+                                    onChange={e => setAddCommentText(e.target.value)}
+                                    onFocus={() => setShowCommentsButtons(true)}
+                                />
+                                <div className="save-button">Save</div>
+                                <div className="cancel-button">Cancel</div>
+                            </AddCommentsForm>
+                                    <div className="comments-tip">Press m for magic</div>
+                            <CommentsDisplay>
+                                {commentsContentRender}
+                            </CommentsDisplay>
                         </div>
                     </div>
                     <div className="right-column">
@@ -541,14 +901,23 @@ const Modal = (props) => {
                             <div className="status-type-picker-title">Status:</div>
                             {statusTypeRender(currentStatusType)}
                         </StatusTypePicker>
-                        <div className="assignees">Assignees: Pickle Rick Baby Yoda Lord Gaben Add More</div>
-                        <div className="reporter">
-                            <div style={{backgroundImage: `url(${gaben})`, backgroundSize: 'cover', width: '24px', height: '24px', display: 'block'}} />&nbsp;
-                            </div>
-                                <div>
 
-                                Reporter: Baby Yoda
-                        </div>
+                        <AssignedUsersPicker
+                            showAssignedUserPicker={showAssignedUserPicker}
+                            ref={assignedUserPicker}
+                        >
+                                <div className="assigned-picker-title">Assignees:</div>
+                                {assignedUserRender(currentAssignedUsers)}
+                        </AssignedUsersPicker>
+
+                        <ReportingUserPicker 
+                            showReportingUserPicker={showReportingUserPicker}
+                            ref={reportingUserPicker}
+                        >
+                            <div className="reporting-user-picker-title">Reporter:</div>
+                            {reportingUserRender(currentReportingUser)}
+                        </ReportingUserPicker>
+
                         <PriorityTypePicker
                             showPriorityPicker={showPriorityPicker}
                             ref={priorityPicker}
